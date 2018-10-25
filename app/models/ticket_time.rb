5c812ef8d56719ebbc58308ccad27ff2ab19a2da
Igor Morse <igormorse@hotmail.com>
2018-10-25 15:20:33 -0300

    fix: Migration and TicketTime Model

--- app/models/ticket_time.rb
+++ app/models/ticket_time.rb
@@ -1,8 +1,8 @@
 class TicketTime < ActiveRecord::Base
   unloadable
   belongs_to(:issue)
-  belongs_to(:custom_value, foreign_key: :time_begin_custom_value_id)
-  belongs_to(:custom_value, foreign_key: :time_end_custom_value_id)
+  belongs_to(:time_begin_custom_value, class_name: :CustomValue, foreign_key: :time_begin_custom_value_id)
+  belongs_to(:time_begin_custom_value, class_name: :CustomValue, foreign_key: :time_end_custom_value_id)
   attr_accessible :time_begin
   attr_accessible :time_end
   attr_accessible :issue_id
--- db/migrate/007_migrate_data_to_custom_values.rb
+++ db/migrate/007_migrate_data_to_custom_values.rb
@@ -13,31 +13,39 @@ class MigrateDataToCustomValues < ActiveRecord::Migration
     puts "*****\n****\n**** \n\tIF ERROR: Configure Time Begin Custom Field in Mega Calendar Plugin Settings!! ****\n****\n*****"
     time_end_cf = CustomField.find(time_end_cf)
 
-    TicketTime.where("time_begin_custom_value_id IS NULL OR time_end_custom_value_id IS NULL").each do |record|
+    TicketTime.where("time_begin_custom_value_id IS NULL OR time_end_custom_value_id IS NULL").each do |ticket_time|
 
-      if (record.time_begin_custom_value_id.nil? && !record.time_begin.nil?)
+      if (ticket_time.time_begin_custom_value_id.nil? && !ticket_time.time_begin.nil?)
 
-        time_begin_cv = CustomValue.find_or_create_by(customized_type: :Issue, customized_id: record.issue_id, custom_field: time_begin_cf, value: record.time_begin.strftime("%H:%M"))
+        time_begin_cv = CustomValue.find_or_create_by(customized_type: :Issue, customized_id: ticket_time.issue_id, custom_field: time_begin_cf, value: ticket_time.time_begin.strftime("%H:%M"))
 
         time_begin_cv.save
 
-        record.time_begin_custom_value_id = time_begin_cv.id
+        ticket_time.time_begin_custom_value_id = time_begin_cv.id
       end
 
-      if (record.time_end_custom_value_id.nil? && !record.time_end.nil?)
+      if (ticket_time.time_end_custom_value_id.nil? && !ticket_time.time_end.nil?)
 
-        time_end_cv = CustomValue.find_or_create_by(customized_type: :Issue, customized_id: record.issue_id, custom_field: time_end_cf, value: record.time_end.strftime("%H:%M"))
+        time_end_cv = CustomValue.find_or_create_by(customized_type: :Issue, customized_id: ticket_time.issue_id, custom_field: time_end_cf, value: ticket_time.time_end.strftime("%H:%M"))
 
         time_end_cv.save
 
-        record.time_end_custom_value_id = time_end_cv.id
+        ticket_time.time_end_custom_value_id = time_end_cv.id
       end
 
-      record.save if !record.time_begin_custom_value_id.nil? || !record.time_end_custom_value_id.nil?
+      ticket_time.save if (!ticket_time.time_begin_custom_value_id.nil? || !ticket_time.time_end_custom_value_id.nil?)
     end
   end
 
-  def def down 
+  def def down
+    
+    tt = TicketTime.where("time_begin_custom_value_id IS NOT NULL AND time_end_custom_value_id IS NOT NULL").first
+    
+    time_begin_cf = tt.time_begin_custom_value.custom_field
+    time_end_cf = tt.time_end_custom_value.custom_field
+
     TicketTime.update_all(time_begin_custom_value_id: nil, time_end_custom_value_id: nil)
+
+    CustomValue.where("custom_field_id = #{time_begin_cf.id} OR custom_field_id = #{time_end_cf.id}").delete_all
   end
 end
